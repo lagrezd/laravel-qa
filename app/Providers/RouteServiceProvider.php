ce98add1f445433e1ed33922015d5a38ee9e060e
Lagreze <lagrezd@gmail.com>
2019-05-21 12:40:53 +0200

    Display answers for question

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,7 +28,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         // Add route show
         Route::bind('slug', function ($slug) {
-            return Question::where('slug', $slug)->first() ?? abort(404);
+            return Question::with('answers.user')->where('slug', $slug)->first() ?? abort(404);
         });
 
         parent::boot();
