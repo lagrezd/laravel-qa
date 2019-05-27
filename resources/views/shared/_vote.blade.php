2c0074423041edbbb0346057fd0b7834c6582213
Lagreze <lagrezd@gmail.com>
2019-05-27 23:29:31 +0200

    Fix -  ->

--- resources/views/shared/_vote.blade.php
+++ resources/views/shared/_vote.blade.php
@@ -18,20 +18,20 @@
 <div class="d-flex flex-column vote-controls">
     <a title="This {{ $name }} is useful"
        class="vote-up {{ Auth::guest() ? 'off': '' }}"
-       onclick="event.preventDefault(); document.getElementById('up-vote-{{ $formID }}').submit();">
+       onclick="event.preventDefault(); document.getElementById('up-vote-{{ $formId }}').submit();">
         <i class="fas fa-caret-up fa-3x"></i>
     </a>
-    <form id="up-vote-{{ $formID }}" action="{{ $formAction }}" method="post" style="display: none">
+    <form id="up-vote-{{ $formId }}" action="{{ $formAction }}" method="post" style="display: none">
         @csrf
         <input type="hidden" name="vote" value="1">
     </form>
     <span class="votes-count">{{ $model->votes_count }}</span>
     <a href="" title="This {{ $name }} is not useful"
        class="vote-down {{ Auth::guest() ? 'off': '' }}"
-       onclick="event.preventDefault(); document.getElementById('down-vote-{{ $formID }}').submit();">
+       onclick="event.preventDefault(); document.getElementById('down-vote-{{ $formId }}').submit();">
         <i class="fas fa-caret-down fa-3x"></i>
     </a>
-    <form id="down-vote-{{ $formID }}" action="{{ $formAction }}" method="post" style="display: none">
+    <form id="down-vote-{{ $formId }}" action="{{ $formAction }}" method="post" style="display: none">
         @csrf
         <input type="hidden" name="vote" value="-1">
     </form>
