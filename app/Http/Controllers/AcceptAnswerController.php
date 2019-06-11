e1320b93b445d540abf644ba51e2b2f3af1c8478
Damien Lagreze <lagrezd@gmail.com>
2019-06-11 16:03:07 +0200

    Create accept answer component

--- app/Http/Controllers/AcceptAnswerController.php
+++ app/Http/Controllers/AcceptAnswerController.php
@@ -18,6 +18,12 @@ class AcceptAnswerController extends Controller
 
         $answer->question->acceptBestAnswer($answer);
 
+        if (request()->expectsJson()) {
+            return response()->json([
+                'message' => "You have accepted this answer as best answer"
+            ]);
+        }
+
         return back();
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,6 +81,6 @@
             'user'       => Auth::user()
         ] )!!}
     </script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
