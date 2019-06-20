737ac5793352f340da90842afc9a23c89079b59c
Damien Lagreze <lagrezd@gmail.com>
2019-06-20 18:58:22 +0200

    Bug update question

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .copy('node_modules/prismjs/themes', 'public/css/prismj-themes')
     .sass('resources/sass/app.scss', 'public/css');
